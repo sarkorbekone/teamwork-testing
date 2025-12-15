18dea059d4d585a07bb4b96a2b716ff2fe4a5a86
Muhammadyusuf <muhammadyusuf120503@gmail.com>
2025-12-15 17:39:24 +0500

    bu test qilish uchun

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ public class Main {
         System.out.println("first team work");
         System.out.println("first team work");
         System.out.println("first team work");
-        for (int i = 0; i < 20; i++) {
-            System.out.println(i);
-
-        }
     }
 }
