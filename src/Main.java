d4e31b6d387a69904dfc5ef60b88067a5499bb3e
sarkorbekone <sarkorbekone@gmail.com>
2025-12-15 17:30:05 +0500

    add for loop

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("first team work");
         System.out.println("first team work");
         System.out.println("first team work");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+
+        }
     }
 }
